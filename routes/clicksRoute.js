0a518381768bc8484a52c17e3b38b254884600fc
alialnabulsi <ali.alnabulsi@std.balamand.edu.lb>
2025-04-20 17:43:04 +0300

    Intialized clicksRoute

---
+++ routes/clicksRoute.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const ClicksController = require('../controllers/clicksController');
+const {} = require('../validators/clicksDTO');
+
+
+const router = express.Router();
