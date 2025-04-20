2ffea3276de888599abdb69db589e3edaf3a4b31
alialnabulsi <ali.alnabulsi@std.balamand.edu.lb>
2025-04-20 17:24:39 +0300

    Intialized AnonymousShortUrlRepository

---
+++ repositories/anonymousShortURLsRepository.js
@@ -0,0 +1,7 @@
+const {User,Click,UserShortUrl,AnonymousShortUrl, sequelize} = require('../models/associations');
+
+class AnonymousShortUrlRepository{
+    
+}
+
+module.exports = AnonymousShortUrlRepository;
