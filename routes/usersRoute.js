1acb416305a39899c519d4365105157a99f4943f
alialnabulsi <ali.alnabulsi@std.balamand.edu.lb>
2025-04-20 17:42:32 +0300

    Intialized usersRoute

---
+++ routes/usersRoute.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const UsersController = require('../controllers/usersController');
+const {} = require('../validators/usersDTO');
+
+
+const router = express.Router();
